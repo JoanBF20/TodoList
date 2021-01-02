fdefd2b8078cdae51b843443c839b014c7435dbb
Joan <joanbarcelo1@paucasesnovescifp.cat>
2021-01-02 21:00:12 +0100

    Creada la clase Task.java
    S'ha creat la clase Task.java que conte un string title i description i un boolean complete, amb els seus getters i setters i constructor

---
+++ app/src/main/java/com/example/todolist/Task.java
@@ -0,0 +1,37 @@
+package com.example.todolist;
+
+public class Task {
+    private String title;
+    private String description;
+    private boolean complete;
+
+    public Task(String title, String description, boolean complete) {
+        this.title = title;
+        this.description = description;
+        this.complete = complete;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isComplete() {
+        return complete;
+    }
+
+    public void setComplete(boolean complete) {
+        this.complete = complete;
+    }
+}
